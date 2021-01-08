57d73a9b173e91f2fb31c55a868077fc3d519b06
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-01-08 18:32:17 +0100

    Temporarily disable git_status

--- home/starship.nix
+++ home/starship.nix
@@ -3,6 +3,8 @@
     enable = true;
     settings = {
       add_newline = false;
+      # git_status  -  634ms <-- facepalm.rs
+      git_status.disabled = true;
       line_break.disabled = true;
       hostname.ssh_only = false;
       username.show_always = true;
