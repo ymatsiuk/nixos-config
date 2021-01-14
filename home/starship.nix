8d5121bc2d747b17bf3b64bdcae6f93ee01b6dbc
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-01-14 08:21:17 +0100

    Adjust starship

--- home/starship.nix
+++ home/starship.nix
@@ -3,10 +3,11 @@
     enable = true;
     settings = {
       add_newline = false;
+      aws.disabled = true;
       # git_status  -  634ms <-- facepalm.rs
-      git_status.disabled = true;
-      line_break.disabled = true;
+      git_status.disabled = false;
       hostname.ssh_only = false;
+      line_break.disabled = true;
       username.show_always = true;
     };
   };
