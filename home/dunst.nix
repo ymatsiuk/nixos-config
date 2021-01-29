ff2b35e77173298056f2ddf24695e256e95f84e6
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-01-29 12:19:58 +0100

    Tune dunst not to cover controls

--- home/dunst.nix
+++ home/dunst.nix
@@ -2,34 +2,42 @@
   services.dunst = {
     enable = true;
     settings = {
-     global = {
-       # browser = "/usr/bin/env firefox -new-tab";
-       #foo
-       follow = "mouse";
-       font = "Iosevka 10";
-       frame_color = "#b16286";
-       frame_width = "2";
-       geometry = "800x10-8+8";
-       horizontal_padding = "2";
-       padding = "2";
-       shrink = false;
-       word_wrap = true;
-     };
-     urgency_critcal = {
-       background = "#3c3836";
-       foreground = "#ebdbb2";
-       timeout = 10;
-     };
-     urgency_low = {
-       background = "#3c3836";
-       foreground = "#ebdbb2";
-       timeout = 10;
-     };
-     urgency_normal = {
-       background = "#3c3836";
-       foreground = "#ebdbb2";
-       timeout = 10;
-     };
+      experimantal = {
+        per_monitor_dpi = true;
+      };
+      global = {
+        corner_radius = 16;
+        follow = "mouse";
+        font = "Iosevka 10";
+        # escape \n with \
+        format = "<b>%s</b>\\n%b";
+        frame_color = "#b16286";
+        frame_width = 8;
+        geometry = "1000x10-40+160";
+        horizontal_padding = 8;
+        icon_position = "off";
+        ignore_newlines = false;
+        markup = "full";
+        padding = 8;
+        show_indicators = false;
+        shrink = false;
+        word_wrap = true;
+      };
+      urgency_critcal = {
+        background = "#3c3836";
+        foreground = "#ebdbb2";
+        timeout = 10;
+      };
+      urgency_low = {
+        background = "#3c3836";
+        foreground = "#ebdbb2";
+        timeout = 10;
+      };
+      urgency_normal = {
+        background = "#3c3836";
+        foreground = "#ebdbb2";
+        timeout = 10;
+      };
    };
  };
 }
