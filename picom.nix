8db23c4cf3c43e5e8134b13cb1db7fb12ea6c816
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-02-17 17:59:17 +0100

    Remove bells and whistles from picom

--- picom.nix
+++ picom.nix
@@ -1,15 +1,11 @@
 {
   services.picom = {
     enable = true;
-    fade = true;
-    fadeDelta = 5;
-    shadow = true;
-    shadowOffsets = [ (-7) (-7) ];
-    shadowOpacity = 0.7;
-    shadowExclude = [ "window_type *= 'normal' && ! name ~= ''" ];
+    fade = false;
+    shadow = false;
     activeOpacity = 1.0;
-    inactiveOpacity = 0.8;
-    menuOpacity = 0.8;
+    inactiveOpacity = 1.0;
+    menuOpacity = 1.0;
     backend = "glx";
     vSync = true;
   };
