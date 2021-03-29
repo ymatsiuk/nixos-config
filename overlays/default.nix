06ab02f1f06efb1fc7a4c1e84085516259b35958
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-03-29 13:38:28 +0200

    Cleaup overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,10 @@
 self: super:
 {
-  fetchFromSourcehut = super.callPackage ./fetchsrht { };
-  dlm = super.callPackage ./dlm { };
   greetd = super.callPackage ./greetd { };
-  gtkgreet = super.callPackage ./gtkgreet { };
   tuigreet = super.callPackage ./tuigreet { };
-  wlgreet = super.callPackage ./wlgreet { };
 
   libfprint-tod = super.callPackage ./libfprint-tod { };
+  fprintd-tod = super.callPackage ./fprintd { libfprint = self.libfprint-tod; };
   libfprint-2-tod1-goodix = super.callPackage ./libfprint-2-tod1-goodix { };
 
   howdy = super.callPackage ./howdy { };
