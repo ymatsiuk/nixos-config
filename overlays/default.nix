29fc1f1f1a2414d54e7b9a9fce69f3f482139c7d
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-04-06 15:47:15 +0200

    Use upstream fprintd pkgs

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,5 @@
 self: super:
 {
-  libfprint-tod = super.callPackage ./libfprint-tod { };
-  fprintd-tod = super.callPackage ./fprintd { libfprint = self.libfprint-tod; };
   libfprint-2-tod1-goodix = super.callPackage ./libfprint-2-tod1-goodix { };
 
   howdy = super.callPackage ./howdy { };
