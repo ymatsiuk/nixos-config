8c0d75cdc51ae51ab996dbd8b28961cc2f0c5f90
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-04-09 18:17:22 +0200

    Override goodix's attributes via overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,10 @@
 self: super:
 {
-  libfprint-2-tod1-goodix = super.callPackage ./libfprint-2-tod1-goodix { };
+  libfprint-2-tod1-goodix = super.libfprint-2-tod1-goodix.overrideAttrs (
+    oldAttrs: rec {
+      passthru.driverPath = "/usr/lib/libfprint-2/tod-1";
+    }
+  );
 
   howdy = super.callPackage ./howdy { };
   pam_python = super.callPackage ./pam_python { };
