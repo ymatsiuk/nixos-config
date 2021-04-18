966e1f467f044c25e2dc2bc2cdaa89bd29bba220
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-04-18 09:25:01 +0200

    Cleanup overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,7 @@
 self: super:
 {
-  libfprint-2-tod1-goodix = super.libfprint-2-tod1-goodix.overrideAttrs (
-    oldAttrs: rec {
-      passthru.driverPath = "/usr/lib/libfprint-2/tod-1";
-    }
-  );
-
   howdy = super.callPackage ./howdy { };
   pam_python = super.callPackage ./pam_python { };
+
+  teleport-ent = super.callPackage ./teleport-ent { };
 }
