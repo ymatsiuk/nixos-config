515d0fcfea787b6351b30a8a60b46c662e3675a1
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-04-21 11:59:39 +0200

    Update bt firmware to latest unreleased

--- configuration.nix
+++ configuration.nix
@@ -53,8 +53,12 @@
   hardware.firmware = with pkgs; [
     sof-firmware
     (firmwareLinuxNonfree.overrideAttrs (oldAttrs: rec {
-      version = "2020-12-18";
-      outputHash = "sha256-hgTfrOmKKpVK+qGuaFtFURLCwcG/cCiT4UYx7qCw+9w=";
+      outputHash = "sha256-97dh9N6KHmK1TfMU0EaNPfZ1Mx7zm/Y3lAfASUSxzXM=";
+      src = pkgs.fetchgit {
+        url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
+        rev = "73144e02dcff54529ab42d951de682740d204180";
+        sha256 = "sha256-6VYtF7MbsCcPFmmOJCapQs9kHZNPRQ9HFZBhTaE2ecE=";
+      };
     }))
   ];
 
