18f6a4983b46e4a685e28642509b0e64b529f6e1
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-04-27 09:25:06 +0200

    Bump firmware

--- configuration.nix
+++ configuration.nix
@@ -53,11 +53,11 @@
   hardware.firmware = with pkgs; [
     sof-firmware
     (firmwareLinuxNonfree.overrideAttrs (oldAttrs: rec {
-      outputHash = "sha256-97dh9N6KHmK1TfMU0EaNPfZ1Mx7zm/Y3lAfASUSxzXM=";
+      outputHash = "sha256-QTz3TS3buDP9VNBmgTeNpJwRBHoQ9GiOd+0mExblPMo=";
       src = pkgs.fetchgit {
         url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-        rev = "73144e02dcff54529ab42d951de682740d204180";
-        sha256 = "sha256-6VYtF7MbsCcPFmmOJCapQs9kHZNPRQ9HFZBhTaE2ecE=";
+        rev = "fa0efeff4894e36b9c3964376f2c99fae101d147";
+        sha256 = "sha256-j9FyiEA918hCycTCZx+vZBukq4fZW1wr9d/OhUULSeI=";
       };
     }))
   ];
