7338712193fb2aa44d75b55f0aba9727cc64433d
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-05-17 15:48:38 +0200

    Drop unused wayland/sway variables

--- greetd.nix
+++ greetd.nix
@@ -2,13 +2,6 @@
 let
   waylandEnablement = pkgs.writeShellScript "wayland-enablement" ''
     export MOZ_ENABLE_WAYLAND=1
-    export CLUTTER_BACKEND=wayland
-    export QT_QPA_PLATFORM=wayland-egl
-    export ECORE_EVAS_ENGINE=wayland-egl
-    export ELM_ENGINE=wayland_egl
-    export SDL_VIDEODRIVER=wayland
-    export _JAVA_AWT_WM_NONREPARENTING=1
-    export NO_AT_BRIDGE=1
   '';
   swayRun = pkgs.writeShellScript "sway-run" ''
     export XDG_SESSION_TYPE=wayland
