9d383d10acf4752e62aa6f0f202c0103bc622608
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-06-09 14:48:34 +0200

    Add LC_TIME locale

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,12 @@
   };
 
   time.timeZone = "Europe/Amsterdam";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_TIME = "nl_NL.UTF-8";
+    };
+  };
 
   environment = {
     homeBinInPath = true;
