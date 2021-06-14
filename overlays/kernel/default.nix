c09e2589b01346030c46f80581b8ab3c0330efba
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-06-14 17:47:55 +0200

    Update kernel

--- overlays/kernel/default.nix
+++ overlays/kernel/default.nix
@@ -1,7 +1,7 @@
 { lib, buildPackages, fetchgit, perl, buildLinux, nixosTests, modDirVersionArg ? null, ... } @ args:
 buildLinux (args // rec {
   version = "5.13.0_drm-tip";
-  modDirVersion = "5.13.0-rc5";
+  modDirVersion = "5.13.0-rc6";
   extraMeta.branch = "5.13";
   ignoreConfigErrors = true;
   enableParallelBuilding = true;
@@ -14,7 +14,7 @@ buildLinux (args // rec {
   '';
   src = fetchgit {
     url = "git://anongit.freedesktop.org/drm-tip";
-    rev = "2bba812cf661cf2ffa2874a30d4a2c8fcb3937cc";
-    sha256 = "sha256-uqR299fGRd0sX8OfUnIPfusE5xpY56Jy+KGINRa8CK4=";
+    rev = "d02d55c2c7574218d5f23a7eaef42c6c2f19805e";
+    sha256 = "sha256-frDe8/yATljvUvjgVgcLw/uG/lfx+425yJi6XJJLxjQ=";
   };
 } // (args.argsOverride or { }))
