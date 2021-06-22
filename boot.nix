3ff8a95691215674da72f2c2520347c6f8ec3839
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-06-22 09:06:11 +0200

    Untaint kernel

--- boot.nix
+++ boot.nix
@@ -4,12 +4,12 @@
   boot.initrd.luks.devices."nixps".device = "/dev/disk/by-uuid/2f7823b9-9e81-4813-8721-55e5000f2c7f";
   boot.kernelParams = [
     "quiet"
-    "i915.modeset=1"
-    "i915.enable_fbc=1"
-    "i915.enable_guc=2"
-    "i915.psr_safest_params=1"
-    "i915.mitigations=off"
-    "mitigations=off"
+    # "i915.modeset=1"
+    # "i915.enable_fbc=1"
+    # "i915.enable_guc=2"
+    # "i915.psr_safest_params=1"
+    # "i915.mitigations=off"
+    # "mitigations=off"
   ];
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
