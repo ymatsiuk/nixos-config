9bf3ed33687d01be453e0e1bf4d94a54b5a66687
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-06-22 18:08:31 +0200

    Cleanup kernel params

--- boot.nix
+++ boot.nix
@@ -2,15 +2,7 @@
   boot.blacklistedKernelModules = [ "psmouse" ];
   boot.initrd.kernelModules = [ "i915" ];
   boot.initrd.luks.devices."nixps".device = "/dev/disk/by-uuid/2f7823b9-9e81-4813-8721-55e5000f2c7f";
-  boot.kernelParams = [
-    "quiet"
-    # "i915.modeset=1"
-    # "i915.enable_fbc=1"
-    # "i915.enable_guc=2"
-    # "i915.psr_safest_params=1"
-    # "i915.mitigations=off"
-    # "mitigations=off"
-  ];
+  boot.kernelParams = [ ];
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 2;
