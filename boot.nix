49bf980c7fd041dd0891581beb95a3312f226386
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-06-23 10:28:51 +0200

    Make boot quiet

--- boot.nix
+++ boot.nix
@@ -2,7 +2,7 @@
   boot.blacklistedKernelModules = [ "psmouse" ];
   boot.initrd.kernelModules = [ "i915" ];
   boot.initrd.luks.devices."nixps".device = "/dev/disk/by-uuid/2f7823b9-9e81-4813-8721-55e5000f2c7f";
-  boot.kernelParams = [ ];
+  boot.kernelParams = [ "quiet" ];
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 2;
