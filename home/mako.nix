ea2203cb172f5d2b2e31e0fe9026d61b002642d9
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-06-24 11:55:03 +0200

    Update mako config

--- home/mako.nix
+++ home/mako.nix
@@ -1,11 +1,10 @@
 {
   programs.mako = {
-    anchor = "center";
     backgroundColor = "#3c3836";
     borderColor = "#b16286";
     borderRadius = 6;
     borderSize = 2;
-    defaultTimeout = 10000;
+    defaultTimeout = 5000;
     enable = true;
     font = "Iosevka 12";
     layer = "overlay";
