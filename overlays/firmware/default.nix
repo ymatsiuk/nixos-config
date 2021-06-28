1f927f5967196d3c2a87ca8101f893206a13d280
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-06-28 13:35:21 +0200

    Add guc_v62 for i915

--- overlays/firmware/default.nix
+++ overlays/firmware/default.nix
@@ -1,12 +1,18 @@
-{ firmwareLinuxNonfree
-, fetchgit
-}:
-firmwareLinuxNonfree.overrideAttrs (oldAttrs: rec {
+{ stdenvNoCC, lib }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "firmware-linux-nonfree";
   version = "2021-06-09";
-  outputHash = "sha256-Sh4jFpz7h2bnmgeXiMM0Kzhk/vxnwIZzOF1M2v3ejUs=";
-  src = fetchgit {
-    url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-    rev = "0f66b74b6267fce66395316308d88b0535aa3df2";
-    sha256 = "sha256-JSLxyf57x4czXZZQcowBlx6X+SK2MMXIoPLXfJIigAI=";
+  src = builtins.fetchGit {
+    url = "git://anongit.freedesktop.org/drm/drm-firmware";
+    ref = "guc_v62";
+    rev = "5b20d5ead3692bf6cdf5b87dedc471391f5b9f1c";
   };
-})
+  installFlags = [ "DESTDIR=$(out)" ];
+  # Firmware blobs do not need fixing and should not be modified
+  dontFixup = true;
+  outputHashMode = "recursive";
+  outputHashAlgo = "sha256";
+  outputHash = "sha256-pl5wCveb3ht1+3lQ8bdK8p7lXTQvsKd+fGRyEGIXy+c=";
+  passthru = { inherit version; };
+}
