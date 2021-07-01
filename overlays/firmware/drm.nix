51fbd47b3de547ce6c8e97c3ec74b9422b580598
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-07-01 09:39:32 +0200

    Update firmware

--- overlays/firmware/drm.nix
+++ overlays/firmware/drm.nix
@@ -2,17 +2,17 @@
 
 stdenvNoCC.mkDerivation rec {
   pname = "drm-firmware";
-  version = "guc_v62";
+  version = "guc_v62_huc_7_9";
   src = builtins.fetchGit {
     url = "git://anongit.freedesktop.org/drm/drm-firmware";
-    ref = "guc_v62";
-    rev = "5b20d5ead3692bf6cdf5b87dedc471391f5b9f1c";
+    ref = "guc_62.0_huc_7.9";
+    rev = "f4d897acd200190350a5f2148316c51c6c57bc9b";
   };
   installFlags = [ "DESTDIR=$(out)" ];
   # Firmware blobs do not need fixing and should not be modified
   dontFixup = true;
   outputHashMode = "recursive";
   outputHashAlgo = "sha256";
-  outputHash = "sha256-pl5wCveb3ht1+3lQ8bdK8p7lXTQvsKd+fGRyEGIXy+c=";
+  outputHash = "sha256-U2flMiVSIsjh3xVI1pNO8SP0eCDF9quZWnUl8l1SN/k=";
 }
 
--- overlays/firmware/git.nix
+++ overlays/firmware/git.nix
@@ -2,14 +2,14 @@
 stdenvNoCC.mkDerivation rec {
   pname = "firmware-linux-nonfree";
   version = "master";
-  outputHash = "sha256-Sh4jFpz7h2bnmgeXiMM0Kzhk/vxnwIZzOF1M2v3ejUs=";
   src = fetchgit {
     url = "https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git";
-    rev = "0f66b74b6267fce66395316308d88b0535aa3df2";
-    sha256 = "sha256-JSLxyf57x4czXZZQcowBlx6X+SK2MMXIoPLXfJIigAI=";
+    rev = "d79c26779d459063b8052b7fe0a48bce4e08d0d9";
+    sha256 = "sha256-ThsEpKjh3uBuXpWnkRvqH0p4WkkHimLUNXAC6Z8gBIU=";
   };
   installFlags = [ "DESTDIR=$(out)" ];
   dontFixup = true;
   outputHashMode = "recursive";
   outputHashAlgo = "sha256";
+  outputHash = "sha256-d/TuDVsRi0URnoqYzJ89fTBpq9m3oxDYFs5pgzThzCo=";
 }
