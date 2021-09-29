faa3227e058510dede354ceab5f9004d5bc045cb
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-09-29 13:18:00 +0200

    Enable iwlwifi power save

--- boot.nix
+++ boot.nix
@@ -4,6 +4,7 @@
   boot.initrd.luks.devices."nixps".device = "/dev/disk/by-uuid/2f7823b9-9e81-4813-8721-55e5000f2c7f";
   boot.extraModprobeConfig = ''
     options cfg80211 ieee80211_regdom="NL"
+    options iwlwifi power_save=1
   '';
   boot.kernelParams = [
     "quiet"
