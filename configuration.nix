c7fd69daaa1ff08c9d4518f74cae32a778d97e89
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-10-08 12:20:41 +0200

    Move pipewire config into include

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
       ./hardware-configuration.nix
       ./nix.nix
       ./opengl.nix
+      ./pipewire.nix
       ./users.nix
     ];
 
@@ -69,24 +70,6 @@
 
   services.fwupd.enable = true;
   services.gnome.gnome-keyring.enable = true;
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    media-session.config.bluez-monitor = {
-      properties = { };
-      rules = [
-        {
-          matches = [{ device.name = "~bluez_card.*"; }];
-          actions.update-props.bluez5.autoswitch-profile = true;
-          actions.update-props.bluez5.auto-connect = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-        }
-        {
-          matches = [{ node.name = "~bluez_input.*"; } { node.name = "~bluez_output.*"; }];
-          actions.update-props.node.pause-on-idle = false;
-        }
-      ];
-    };
-  };
   services.thermald.enable = true;
   services.upower.enable = true;
 
