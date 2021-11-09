8202c9cb2335ddc3dd007b2f4c94d6955750ab9d
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-11-09 14:02:50 +0100

    Add iwd and ell

--- flake.nix
+++ flake.nix
@@ -45,6 +45,8 @@
       firefox = final.firefox-bin.override { forceWayland = true; };
       iwlwifi-firmware = final.callPackage ./overlays/firmware.nix { };
       slack = final.callPackage ./overlays/slack.nix { forceWayland = true; enablePipewire = true; };
+      ell-45 = final.callPackage ./overlays/ell.nix { };
+      iwd = final.callPackage ./overlays/iwd.nix { };
     };
 
     packages.x86_64-linux = (builtins.head (builtins.attrValues inputs.self.nixosConfigurations)).pkgs;
