56c703caa62d2d442f91982906b38dce227af9de
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-11-24 08:00:11 +0100

    Make git fetch prune by default

--- home/git.nix
+++ home/git.nix
@@ -35,6 +35,7 @@
     signing = { signByDefault = true; };
     extraConfig = {
       pull = { rebase = false; };
+      fetch = { prune = true; };
       credential = { helper = "lastpass"; };
       rebase = {
         autoStash = true;
