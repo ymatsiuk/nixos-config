fa92cf950738dc13b463ba2d28c1259c330d24e7
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-12-07 13:48:05 +0100

    Update kernel

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -3,16 +3,9 @@ buildLinux (args // rec {
   version = "5.16-rc3";
   modDirVersion = builtins.replaceStrings [ "-" ] [ ".0-" ] version;
   extraMeta.branch = lib.versions.majorMinor version;
-  structuredExtraConfig = with lib.kernel; {
-    DEBUG_LIST = yes;
-    SOFTLOCKUP_DETECTOR = yes;
-    HARDLOCKUP_DETECTOR = yes;
-    DETECT_HUNG_TASK = yes;
-    WQ_WATCHDOG = yes;
-  };
   src = fetchgit {
     url = "git://anongit.freedesktop.org/drm-tip";
-    rev = "a45a9993b4633973f97a4b981ceeb23e4b0963f9";
-    sha256 = "sha256-3kupewh27sjBO1GziqmjvREcWZ5IUEhaQJMxLND0URU=";
+    rev = "909bf926eaf382123d9b215871143d9e3cf44aa3";
+    sha256 = "sha256-SzJXa51mztY5OIi+WnooTduRLIukpuNzzVjBRVcOF9w=";
   };
 } // (args.argsOverride or { }))
