8a9302f9368373adfa7f30e00d064d949342c5e4
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2021-12-28 09:43:22 +0100

    Upgrade kernel

--- overlays/kernel.nix
+++ overlays/kernel.nix
@@ -1,11 +1,11 @@
 { lib, fetchgit, buildLinux, ... } @ args:
 buildLinux (args // rec {
-  version = "5.16-rc6";
+  version = "5.16-rc7";
   modDirVersion = builtins.replaceStrings [ "-" ] [ ".0-" ] version;
   extraMeta.branch = lib.versions.majorMinor version;
   src = fetchgit {
     url = "git://anongit.freedesktop.org/drm-tip";
-    rev = "348c868d115c18195f5cf5b7a53b6f1370eab954";
-    sha256 = "sha256-Ztz/YiWZM/EEnOJRj3HQjz6UxDz5O01/Ao0EHaeoab4=";
+    rev = "6ff0b924d73f6fa1f08bef4fce0f7342b2523c72";
+    sha256 = "sha256-3jbq2WIOVSbf4rg2ZngfkRg4msNsT9vPh8r+OtPOppM=";
   };
 } // (args.argsOverride or { }))
