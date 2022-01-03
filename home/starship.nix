9710f11dcbbd9a8a02d63fd1d8a8eafd98f3a0b5
Yurii Matsiuk <ymatsiuk@users.noreply.github.com>
2022-01-03 16:41:22 +0100

    Starship: drop excessive config

--- home/starship.nix
+++ home/starship.nix
@@ -4,10 +4,7 @@
     settings = {
       add_newline = false;
       aws.disabled = true;
-      hostname.ssh_only = false;
       line_break.disabled = true;
-      username.format = "[$user]($style) at ";
-      username.show_always = true;
     };
   };
 }
