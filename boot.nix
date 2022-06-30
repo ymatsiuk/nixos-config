ca8dcb7eb676cc13c894443a293b6361c22b9712
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2022-06-30 12:20:56 +0200

    Switch to initrd with systemd

--- boot.nix
+++ boot.nix
@@ -11,6 +11,12 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ "i915" "v4l2loopback" ];
   boot.initrd.luks.devices."nixps".device = "/dev/disk/by-uuid/2f7823b9-9e81-4813-8721-55e5000f2c7f";
+  boot.initrd.systemd = {
+    enable = true;
+    emergencyAccess = true;
+    additionalUpstreamUnits = [ "systemd-vconsole-setup.service" ];
+    storePaths = [ config.console.font ];
+  };
   boot.kernelModules = [ "kvm-intel" ];
   boot.kernelParams = [
     "quiet"
