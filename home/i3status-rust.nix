ca9492817a5ac53feb00f44e2c245f90467ac137
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2022-08-15 17:52:25 +0200

    i3status-rust: fold and cleanup options

--- home/i3status-rust.nix
+++ home/i3status-rust.nix
@@ -34,41 +34,19 @@ in
     bars = {
       bottom = {
         blocks = [
-          # { block = "custom"; cycle = [ stand sit ]; json = true; }
           { block = "custom"; command = kernel; json = true; interval = "once"; }
           { block = "net"; hide_missing = true; hide_inactive = true; device = "tun0"; format = " "; }
           { block = "custom"; command = checkNixosUpdates; json = true; interval = 300; }
           { block = "uptime"; }
           { block = "bluetooth"; mac = "CC:98:8B:93:08:1F"; hide_disconnected = true; format = "$percentage|"; }
           { block = "bluetooth"; mac = "18:B9:6E:D8:41:A9"; hide_disconnected = true; format = "$percentage|"; }
-          {
-            block = "toggle";
-            text = "ldac";
-            command_state = "${a2dpIsActive}";
-            command_on = "${setProfile} 10";
-            command_off = "${setProfile} 260";
-            interval = 5;
-          }
           { block = "cpu"; }
           { block = "net"; device = "wlan0"; format = "$signal_strength"; }
           { block = "backlight"; }
-          { block = "temperature"; }
-          {
-            block = "sound";
-            driver = "pulseaudio";
-            click = [
-              { button = "left"; cmd = "pavucontrol"; }
-            ];
-          }
+          { block = "temperature"; collapsed = false; }
+          { block = "sound"; driver = "pulseaudio"; click = [{ button = "left"; cmd = "pavucontrol"; }]; }
           { block = "battery"; driver = "upower"; device = "BAT0"; }
-          {
-            block = "time";
-            locale = "nl_NL";
-            format = "%a %e %b %R";
-            click = [
-              { button = "left"; cmd = "gsimplecal"; }
-            ];
-          }
+          { block = "time"; locale = "nl_NL"; format = "%a %e %b %R"; click = [{ button = "left"; cmd = "gsimplecal"; }]; }
         ];
         settings = {
           theme = {
