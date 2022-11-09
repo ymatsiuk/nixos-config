c93a9530b94a1d96f219bf42cd1e160c9f0fdcb5
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2022-11-09 17:22:05 +0100

    kanshi: unbreak config

--- home/kanshi.nix
+++ home/kanshi.nix
@@ -18,7 +18,7 @@
             status = "disable";
           }
           {
-            criteria = "LG Electronics LG HDR 4K 0x0000C950";
+            criteria = "Goldstar Company Ltd LG HDR 4K 0x0000C950";
             status = "enable";
             scale = 2.0;
           }
