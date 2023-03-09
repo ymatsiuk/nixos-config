d13fbc0cc26d351cad5267536f22485452bae361
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2023-03-09 14:07:38 +0100

    Add focking gcloud and focking zoom

--- hm-cli.nix
+++ hm-cli.nix
@@ -17,12 +17,15 @@
     exa
     fd
     gcc
+    (google-cloud-sdk.withExtraComponents ([
+      google-cloud-sdk.components.gke-gcloud-auth-plugin
+      google-cloud-sdk.components.kubectl
+    ]))
     gnumake
     go
     golangci-lint
     jq
     kind
-    kubectl
     kubernetes-helm
     kustomize
     lastpass-cli
@@ -34,6 +37,7 @@
     terraform
     vault
     yq-go
+    zoom-us
   ];
   home.stateVersion = "22.05";
 }
