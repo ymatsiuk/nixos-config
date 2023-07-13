d5ad1b94158c151bf67fa2f7e0b5079fd76e40bc
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2023-07-13 10:50:17 +0200

    wezterm: customize config to match alacritty

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -1,3 +1,20 @@
 {
-  programs.wezterm.enable = true;
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+      local config = {}
+
+      if wezterm.config_builder then
+        config = wezterm.config_builder()
+      end
+
+      config.color_scheme = 'Gruvbox Dark (Gogh)'
+      config.enable_tab_bar = false
+      config.font = wezterm.font 'Iosevka'
+      config.term = 'wezterm'
+      config.window_padding = { left = 2, right = 2, top = 2, bottom = 2, }
+
+      return config
+    '';
+  };
 }
