562f334986d479c8ac4cb21fdde3314fba6e2966
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2023-10-04 15:57:33 +0200

    neovim: update indent_blankline config

--- home/neovim.nix
+++ home/neovim.nix
@@ -112,9 +112,9 @@
           vim.opt.listchars:append "space:⋅"
           vim.opt.listchars:append "eol:↴"
 
-          require("indent_blankline").setup {
-              show_end_of_line = true,
-              space_char_blankline = " ",
+          require("ibl").setup{
+            show_end_of_line = true,
+            space_char_blankline = " ",
           }
         '';
       }
