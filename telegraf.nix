eb52841c40cc4985c97710da65be2c00bb5e267a
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2023-10-17 11:45:03 +0200

    Update telegraf config

--- telegraf.nix
+++ telegraf.nix
@@ -6,37 +6,14 @@ in
     enable = true;
     extraConfig = {
       inputs = {
-        cpu = {
-          percpu = true;
-          totalcpu = true;
-          collect_cpu_time = false;
-          report_active = false;
-        };
-        disk = {
-          ignore_fs = [
-            "tmpfs"
-            "devtmpfs"
-            "devfs"
-            "iso9660"
-            "overlay"
-            "aufs"
-            "squashfs"
-          ];
-        };
-        diskio = { };
-        mem = { };
-        nstat = { };
-        processes = { };
-        swap = { };
         system = { };
-        temp = { };
       };
       outputs = {
         influxdb_v2 = {
           urls = [ "http://nixpi4:8086" ];
-          token = secrets.telegraf.INFLUX_TOKEN;
-          organization = "home";
-          bucket = "xps";
+          token = secrets.telegraf.system.token;
+          organization = secrets.telegraf.system.org;
+          bucket = secrets.telegraf.system.bucket;
         };
       };
     };
