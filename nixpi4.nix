679b13dd7abd65cf86191ba51c3d7f18846c5b8e
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-02-27 11:44:06 +0100

    Format nixpi4.nix

--- nixpi4.nix
+++ nixpi4.nix
@@ -6,26 +6,22 @@
     ./telegraf.nix
   ];
 
-  boot = {
-    extraModprobeConfig = ''
-      options cfg80211 ieee80211_regdom="NL"
-    '';
-  };
+  boot.extraModprobeConfig = ''
+    options cfg80211 ieee80211_regdom="NL"
+  '';
 
-  fileSystems."/" =
-    {
+  fileSystems = {
+    "/" = {
       device = "/dev/disk/by-uuid/a58ae62a-1358-42cb-81c9-971fdef73510";
       fsType = "ext4";
     };
-
-  fileSystems."/boot" =
-    {
+    "/boot" = {
       device = "/dev/disk/by-uuid/1C3B-1E13";
       fsType = "vfat";
     };
+  };
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/4918387b-8275-4f05-9938-b18ac7ca2df0"; }];
+  swapDevices = [{ device = "/dev/disk/by-uuid/4918387b-8275-4f05-9938-b18ac7ca2df0"; }];
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
