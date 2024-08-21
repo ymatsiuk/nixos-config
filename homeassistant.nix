67c6d926e8ef2f50f7278f5467ac090998058e99
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-08-21 14:59:52 +0200

    home-assistant: 2024.8.1 -> 2024.8.2

--- homeassistant.nix
+++ homeassistant.nix
@@ -392,7 +392,7 @@ in
         environment = {
           TZ = "Europe/Amsterdam";
         };
-        image = "ghcr.io/home-assistant/home-assistant:2024.8.1";
+        image = "ghcr.io/home-assistant/home-assistant:2024.8.2";
         extraOptions = [
           "--device=/dev/ttyACM0"
           "--device=/dev/ttyUSB0"
