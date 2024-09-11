ac1ea16f22f73854e830a36af81b84311145f4ac
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-09-11 16:35:13 +0200

    alacritty: fix deprecation warning

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -4,6 +4,7 @@
     enable = true;
     settings = {
       colors = {
+        draw_bold_text_with_bright_colors = true;
         primary = {
           background = "0x282828";
           foreground = "0xebdbb2";
@@ -32,7 +33,6 @@
       env.TERM = "xterm-256color";
       font = {
         size = 12;
-        draw_bold_text_with_bright_colors = true;
       };
       hints = {
         alphabet = "jfkdls;ahgurieowpq";
