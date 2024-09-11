a037299b8763498fc496b081c02089bb90be0236
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-09-11 16:35:46 +0200

    foot: replace colors with theme include

--- home/foot.nix
+++ home/foot.nix
@@ -1,34 +1,18 @@
+{ pkgs, ... }:
 {
   programs.foot = {
     enable = true;
     server.enable = true;
     settings = {
       main = {
+        include = "${pkgs.foot.themes}/share/foot/themes/gruvbox-dark";
+        term = "xterm-256color";
         font = "Iosevka:size=12";
+        dpi-aware = "yes";
       };
       scrollback = {
         lines = 5000;
         multiplier = 5;
-      };
-      colors = {
-        background = "282828";
-        foreground = "ebdbb2";
-        regular0 = "282828";
-        regular1 = "cc241c";
-        regular2 = "98971a";
-        regular3 = "d79921";
-        regular4 = "458588";
-        regular5 = "b16286";
-        regular6 = "689d6a";
-        regular7 = "a89984";
-        bright0 = "928374";
-        bright1 = "fb4934";
-        bright2 = "b8bb26";
-        bright3 = "fabd2f";
-        bright4 = "83a598";
-        bright5 = "d3869b";
-        bright6 = "8ec07c";
-        bright7 = "ebdbb2";
       };
     };
   };
