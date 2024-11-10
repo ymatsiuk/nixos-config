76fc163e59dea1a88dd73915f120607e3efb7e09
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-11-10 18:15:33 +0100

    neovim: add autoformat for terraform

--- home/neovim.nix
+++ home/neovim.nix
@@ -231,6 +231,14 @@
               OrgImports(1000)
             end,
           })
+
+          -- Autoformat terraform files on save
+          vim.api.nvim_create_autocmd({"BufWritePre"}, {
+            pattern = {"*.tf", "*.tfvars"},
+            callback = function()
+              vim.lsp.buf.format()
+            end,
+          })
         '';
       }
     ];
