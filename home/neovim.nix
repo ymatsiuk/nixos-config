75285269f4c63d2e7bc16f3b2d84aaf6e8b845dd
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-11-25 20:07:27 +0100

    neovim: make goimport work

--- home/neovim.nix
+++ home/neovim.nix
@@ -6,6 +6,7 @@
     vimAlias = true;
     extraPackages = with pkgs; [
       gopls
+      gotools
       nil
       pyright
       nodePackages.bash-language-server
@@ -210,26 +211,23 @@
             end,
           })
 
-          function OrgImports(wait_ms)
-            local params = vim.lsp.util.make_range_params()
-            params.context = {only = {"source.organizeImports"}}
-            local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-            for _, res in pairs(result or {}) do
-              for _, r in pairs(res.result or {}) do
-                if r.edit then
-                  vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-                else
-                  vim.lsp.buf.execute_command(r.command)
+          -- Autoimport and autoformat for Go files
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+              local params = vim.lsp.util.make_range_params()
+              params.context = {only = {"source.organizeImports"}}
+              local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+              for cid, res in pairs(result or {}) do
+                for _, r in pairs(res.result or {}) do
+                  if r.edit then
+                    local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+                    vim.lsp.util.apply_workspace_edit(r.edit, enc)
+                  end
                 end
               end
+              vim.lsp.buf.format({async = false})
             end
-          end
-
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = { "*.go" },
-            callback = function()
-              OrgImports(1000)
-            end,
           })
 
           -- Autoformat terraform files on save
