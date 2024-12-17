7367116f83b1f277f4fdc08fb600d771657431f0
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-12-17 12:32:59 +0100

    starhip: enable kubernetes

--- home/starship.nix
+++ home/starship.nix
@@ -6,6 +6,16 @@
       aws.disabled = true;
       gcloud.disabled = true;
       line_break.disabled = true;
+      kubernetes = {
+        disabled = false;
+        contexts = [
+          {
+            context_pattern = "arn:aws:eks:(?P<region>[\\w-]+):(?P<account_id>[\\d]+):cluster/(?P<cluster>[\\w-]+)";
+            context_alias = "$cluster";
+            style = "green";
+          }
+        ];
+      };
     };
   };
 }
