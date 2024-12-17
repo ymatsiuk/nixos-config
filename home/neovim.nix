e1156dc375c9bc0ecc9995137f69c5c2a9458752
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-12-17 13:13:20 +0100

    neovim: update config

--- home/neovim.nix
+++ home/neovim.nix
@@ -17,6 +17,9 @@
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
 
+      -- Set the leader key
+      vim.g.mapleader = " "
+
       -- Options
       vim.opt.backup = false
       vim.opt.clipboard = "unnamed,unnamedplus"
@@ -35,9 +38,9 @@
       vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
       vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
       -- Insert into selection without copying it
-      vim.keymap.set("x", "<space>p", [["_dP]])
+      vim.keymap.set("x", "<leader>p", [["_dP]])
       -- Delete without placing into the register
-      vim.keymap.set({"n", "v"}, "<space>d", [["_d]])
+      vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
       -- Don't jump when merge lines
       vim.keymap.set("n", "J", "mzJ`z")
       -- Stay in the middle of the screen while scrolling
@@ -51,8 +54,8 @@
       -- Disable space in normal/visual modes
       vim.keymap.set({ "n", "v" }, "<space>", "<nop>", { silent = true })
       -- Toggle nvim-tree
-      vim.keymap.set({ "n" }, "<space>tt", ":NvimTreeToggle<CR>", { silent = true })
-      vim.keymap.set({ "n" }, "<space>tf", ":NvimTreeFocus<CR>", { silent = true })
+      vim.keymap.set({ "n" }, "<leader>tt", ":NvimTreeToggle<CR>", { silent = true })
+      vim.keymap.set({ "n" }, "<leader>tf", ":NvimTreeFocus<CR>", { silent = true })
 
       -- Remove trailing white spaces on save
       vim.api.nvim_create_autocmd("BufWritePre", {
@@ -67,6 +70,65 @@
       vim-nix
       vim-sleuth
       vim-terraform
+      {
+        plugin = noice-nvim;
+        type = "lua";
+        config = ''
+          require("noice").setup()
+        '';
+      }
+      {
+        plugin = gitsigns-nvim;
+        type = "lua";
+        config = ''
+          require("gitsigns").setup({
+            on_attach = function(bufnr)
+              local gitsigns = require('gitsigns')
+
+              local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+              end
+
+              -- Navigation
+              map('n', ']c', function()
+                if vim.wo.diff then
+                  vim.cmd.normal({']c', bang = true})
+                else
+                  gitsigns.nav_hunk('next')
+                end
+              end)
+
+              map('n', '[c', function()
+                if vim.wo.diff then
+                  vim.cmd.normal({'[c', bang = true})
+                else
+                  gitsigns.nav_hunk('prev')
+                end
+              end)
+
+              -- Actions
+              map('n', '<leader>hs', gitsigns.stage_hunk)
+              map('n', '<leader>hr', gitsigns.reset_hunk)
+              map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+              map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+              map('n', '<leader>hS', gitsigns.stage_buffer)
+              map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+              map('n', '<leader>hR', gitsigns.reset_buffer)
+              map('n', '<leader>hp', gitsigns.preview_hunk)
+              map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+              map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+              map('n', '<leader>hd', gitsigns.diffthis)
+              map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+              map('n', '<leader>td', gitsigns.toggle_deleted)
+
+              -- Text object
+              map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+            end
+          })
+        '';
+      }
       {
         plugin = nvim-web-devicons;
         type = "lua";
@@ -149,11 +211,11 @@
         config = ''
           require("telescope").setup()
           local builtin = require("telescope.builtin")
-          vim.keymap.set("n", "<space>ff", builtin.find_files, { desc = "Telescope find files" })
-          vim.keymap.set("n", "<space>fg", builtin.live_grep, { desc = "Telescope live grep" })
-          vim.keymap.set("n", "<space>fb", builtin.buffers, { desc = "Telescope buffers" })
-          vim.keymap.set("n", "<space>fc", builtin.current_buffer_fuzzy_find, { desc = "Telescope current buffer" })
-          vim.keymap.set("n", "<space>fh", builtin.help_tags, { desc = "Telescope help tags" })
+          vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+          vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+          vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+          vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, { desc = "Telescope current buffer" })
+          vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
         '';
       }
       {
@@ -179,10 +241,10 @@
 
           -- Global mappings.
           -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-          vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+          vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
           vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
           vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-          vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+          vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
           -- Use LspAttach autocommand to only map the following keys
           -- after the language server attaches to the current buffer
@@ -200,16 +262,16 @@
               vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
               vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
               vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-              vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-              vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-              vim.keymap.set("n", "<space>wl", function()
+              vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+              vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+              vim.keymap.set("n", "<leader>wl", function()
                 print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
               end, opts)
-              vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-              vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-              vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+              vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+              vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+              vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
               vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-              vim.keymap.set("n", "<space>f", function()
+              vim.keymap.set("n", "<leader>f", function()
                 vim.lsp.buf.format { async = true }
               end, opts)
             end,
