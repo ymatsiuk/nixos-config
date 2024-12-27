c3d484e2a6718ae6530a81019176379c18e1da7a
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2024-12-27 16:17:19 +0100

    neovim: update config

--- home/neovim.nix
+++ home/neovim.nix
@@ -307,6 +307,14 @@
               vim.lsp.buf.format()
             end,
           })
+
+          -- Use spelling for markdown files ']s' to find next, '[s' for previous, 'z=' for suggestions when on one.
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "html", "markdown", "text" },
+            callback = function()
+              vim.opt_local.spell = true
+            end,
+          })
         '';
       }
     ];
