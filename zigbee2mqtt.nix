d41bd98040fabcec4f2858ddab3792432f2ba54c
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2025-02-04 09:32:29 +0100

    Add zigbee2mqtt

---
+++ zigbee2mqtt.nix
@@ -0,0 +1,31 @@
+{
+  services.zigbee2mqtt = {
+    enable = true;
+    settings = {
+      advanced = {
+        channel = 25;
+        device_options.legacy = false;
+        homeassistant_legacy_entity_attributes = false;
+        homeassistant_legacy_triggers = false;
+        legacy_api = false;
+        legacy_availability_payload = false;
+        transmit_power = 20;
+      };
+      devices = "devices.yaml";
+      frontend.enabled = true;
+      groups = "groups.yaml";
+      homeassistant.enabled = true;
+      mqtt = {
+        base_topic = "zigbee2mqtt";
+        server = "mqtt://nixpi4:1883";
+      };
+      permit_join = false;
+      serial = {
+        adapter = "ember";
+        baudrate = 115200;
+        port = "tcp://slzb-06m.local:6638";
+      };
+      version = 4;
+    };
+  };
+}
