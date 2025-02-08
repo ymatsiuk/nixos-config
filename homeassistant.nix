7f441dafc7a6fdf99d92961915d9d886949e20d6
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2025-02-08 12:39:44 +0100

    home-assistant: version bump

--- homeassistant.nix
+++ homeassistant.nix
@@ -811,7 +811,7 @@ in
         environment = {
           TZ = "Europe/Amsterdam";
         };
-        image = "ghcr.io/home-assistant/home-assistant:2025.1.4";
+        image = "ghcr.io/home-assistant/home-assistant:2025.2.1";
         extraOptions = [
           "--device=/dev/ttyACM0"
           "--device=/dev/ttyUSB0"
