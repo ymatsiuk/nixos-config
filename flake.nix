5e70f0c0b0b3070cabe1ae2e06bfea17e78edeee
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2025-03-10 19:15:39 +0100

    Remove nixpkgs-wayland

--- flake.nix
+++ flake.nix
@@ -8,8 +8,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-    nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
   };
 
   outputs =
@@ -20,7 +18,6 @@
       home-manager,
       flake-utils,
       nixos-hardware,
-      nixpkgs-wayland,
     }:
     let
       makeOpinionatedNixpkgs =
@@ -46,16 +43,6 @@
             {
               nix.extraOptions = "experimental-features = nix-command flakes";
               nix.registry.nixpkgs.flake = nixpkgs;
-              nix.settings = {
-                trusted-public-keys = [
-                  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                  "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-                ];
-                substituters = [
-                  "https://cache.nixos.org"
-                  "https://nixpkgs-wayland.cachix.org"
-                ];
-              };
               nixpkgs = {
                 inherit pkgs;
               };
@@ -70,7 +57,6 @@
           overlays = [
             nur.overlays.default
             self.overlays.wrk
-            nixpkgs-wayland.overlay
           ];
           modules = [
             ./nixps.nix
