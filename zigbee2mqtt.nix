f17a27ee1605ce5c4ad38b07cc3451e12bbe459b
Yurii Matsiuk <24990891+ymatsiuk@users.noreply.github.com>
2026-01-31 19:45:15 +0100

    Move mosquitto, zigbee2mqtt into rootless podman

--- zigbee2mqtt.nix
+++ zigbee2mqtt.nix
@@ -1,39 +1,158 @@
+{ pkgs, ... }:
+let
+  secrets = import ./secrets.nix;
+  yaml = pkgs.formats.yaml { };
+in
 {
-  systemd.services.zigbee2mqtt.after = [ "mosquitto.service" ];
-  services.zigbee2mqtt = {
-    enable = true;
-    settings = {
-      advanced = {
-        channel = 25;
-        device_options.legacy = false;
-        homeassistant_legacy_entity_attributes = false;
-        homeassistant_legacy_triggers = false;
-        legacy_api = false;
-        legacy_availability_payload = false;
-        last_seen = "ISO_8601";
-        transmit_power = 20;
+  virtualisation.oci-containers.containers = {
+    # Z2M for Deconz USB stick
+    zigbee2mqtt_deconz = {
+      dependsOn = [ "mosquitto" ];
+      image = "ghcr.io/koenkk/zigbee2mqtt:2.7.2";
+      environment.TZ = "Europe/Amsterdam";
+      podman = {
+        sdnotify = "healthy";
+        user = "ymatsiuk";
       };
-      availability.enabled = true;
-      devices = "devices.yaml";
-      frontend.enabled = true;
-      groups = "groups.yaml";
-      homeassistant = {
-        enabled = true;
-        experimental_event_entities = true;
-        status_topic = "homeassistant/status";
+      volumes =
+        let
+          deconz = yaml.generate "deconz.yaml" {
+            frontend = {
+              enabled = true;
+              port = 8080;
+            };
+            mqtt = {
+              base_topic = "zigbee2mqtt_deconz";
+              server = "mqtt://nixlab.local:1883";
+              version = 5;
+            };
+            permit_join = false;
+            homeassistant = {
+              enabled = true;
+              experimental_event_entities = true;
+            };
+            serial = {
+              port = "/dev/ttyACM0";
+              adapter = "deconz";
+              baudrate = 115200;
+              rtscts = false;
+            };
+            version = 4;
+            advanced = {
+              log_level = "debug";
+              channel = 25;
+              last_seen = "ISO_8601";
+              network_key = secrets.zigbee2mqtt.deconz.network_key;
+              pan_id = secrets.zigbee2mqtt.deconz.pan_id;
+              ext_pan_id = secrets.zigbee2mqtt.deconz.ext_pan_id;
+            };
+            devices = "devices.yaml";
+            groups = "groups.yaml";
+            availability = {
+              enabled = true;
+            };
+          };
+        in
+        [
+          "/run/dbus:/run/dbus:ro"
+          "/home/ymatsiuk/zigbee2mqtt/deconz:/app/data"
+          "${deconz}:/app/data/configuration.yaml:ro"
+        ];
+      ports = [ "9090:8080" ];
+      devices = [
+        "/dev/serial/by-id/usb-dresden_elektronik_ingenieurtechnik_GmbH_ConBee_II_DE2598392-if00:/dev/ttyACM0"
+      ];
+      extraOptions = [
+        "--cap-drop=ALL"
+        "--userns=keep-id"
+        "--group-add=keep-groups"
+        "--health-cmd"
+        "wget -qO /dev/null http://localhost:8080 || exit 1"
+      ];
+    };
+    # Z2M for SLZB-06M
+    zigbee2mqtt_slzb06m = {
+      dependsOn = [ "mosquitto" ];
+      image = "ghcr.io/koenkk/zigbee2mqtt:2.7.2";
+      environment.TZ = "Europe/Amsterdam";
+      podman = {
+        sdnotify = "healthy";
+        user = "ymatsiuk";
       };
-      mqtt = {
-        base_topic = "zigbee2mqtt";
-        server = "mqtt://nixlab.local:1883";
-        version = 5;
+      volumes =
+        let
+          slzb06m = yaml.generate "slzb06m.yaml" {
+            advanced = {
+              log_level = "debug";
+              channel = 25;
+              last_seen = "ISO_8601";
+              transmit_power = 20;
+              network_key = secrets.zigbee2mqtt.slzb06m.network_key;
+              pan_id = secrets.zigbee2mqtt.slzb06m.pan_id;
+              ext_pan_id = secrets.zigbee2mqtt.slzb06m.ext_pan_id;
+            };
+            availability.enabled = true;
+            frontend.enabled = true;
+            homeassistant = {
+              enabled = true;
+              experimental_event_entities = true;
+            };
+            mqtt = {
+              base_topic = "zigbee2mqtt_slzb06m";
+              server = "mqtt://nixlab.local:1883";
+              version = 5;
+            };
+            serial = {
+              adapter = "ember";
+              baudrate = 115200;
+              port = "tcp://slzb-06m.lan:6638";
+            };
+            version = 4;
+            devices = "devices.yaml";
+            groups = "groups.yaml";
+          };
+        in
+        [
+          "/run/dbus:/run/dbus:ro"
+          "/home/ymatsiuk/zigbee2mqtt/slzb06m:/app/data"
+          "${slzb06m}:/app/data/configuration.yaml:ro"
+        ];
+      ports = [ "8090:8080" ];
+      extraOptions = [
+        "--cap-drop=ALL"
+        "--userns=keep-id"
+        "--group-add=keep-groups"
+        "--health-cmd"
+        "wget -qO /dev/null http://localhost:8080 || exit 1"
+      ];
+    };
+    # Mosquitto
+    mosquitto = {
+      image = "docker.io/eclipse-mosquitto:2.0.22"; # itho doesn't support 2.1.x yet
+      podman = {
+        sdnotify = "healthy";
+        user = "ymatsiuk";
       };
-      permit_join = false;
-      serial = {
-        adapter = "ember";
-        baudrate = 115200;
-        port = "tcp://slzb-06m.local:6638";
-      };
-      version = 4;
+      extraOptions = [
+        "--cap-drop=ALL"
+        "--userns=keep-id"
+        "--health-cmd"
+        "mosquitto_sub -t '$SYS/#' -C 1 -i healthcheck -W 3 || exit 1" # https://github.com/eclipse-mosquitto/mosquitto/issues/1270#issuecomment-1478648748
+      ];
+      ports = [
+        "1883:1883/tcp"
+      ];
+      volumes =
+        let
+          config = pkgs.writeText "mosquitto.conf" ''
+            listener 1883
+            allow_anonymous true
+          '';
+        in
+        [
+          "${config}:/mosquitto/config/mosquitto.conf:ro"
+          "/home/ymatsiuk/mosquitto:/mosquitto"
+        ];
     };
   };
 }
